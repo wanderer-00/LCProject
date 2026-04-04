a0723d6141fded5daadda5ce06aff0302ef5890a
wanderer <56039464+wanderer-00@users.noreply.github.com>
2026-04-04 20:50:46 +0300

    add FPS display

---
+++ script/fps.js
@@ -0,0 +1,19 @@
+let lastTime = performance.now();
+let frameCount = 0;
+const fpsDisplay = document.getElementById('fps-counter');
+
+function updateFPS() {
+    const now = performance.now();
+    frameCount++;
+
+    if (now >= lastTime + 1000) {
+        const fps = Math.round((frameCount * 1000) / (now - lastTime));
+        fpsDisplay.innerText = `FPS: ${fps}`;
+        frameCount = 0;
+        lastTime = now;
+    }
+
+    requestAnimationFrame(updateFPS);
+}
+
+requestAnimationFrame(updateFPS);
